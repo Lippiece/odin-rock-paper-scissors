c9e6f1c0a892548cf01f721d9d58e581f8f7a35b
Lippiece <seriouss7222@gmail.com>
2022-04-02 13:19:12 +0200

    Format, add div variable

--- scripts.js
+++ scripts.js
@@ -1,100 +1,106 @@
 let score;
+let div = document.querySelector("div");
 
-let computerPlay = (result) => {
-  let index = Math.floor(Math.random() * 3)
-  switch (index) {
-    case 0:
-      result = "Rock";
-      break;
-    case 1:
-      result = "Paper";
-      break;
-    case 2:
-      result = "Scissors";
-      break;
-  }
-  return result;
+let computerPlay = result => {
+	let index = Math.floor(Math.random() * 3);
+	switch (index) {
+		case 0:
+			result = "Rock";
+			break;
+		case 1:
+			result = "Paper";
+			break;
+		case 2:
+			result = "Scissors";
+			break;
+	}
+	return result;
 };
 
 let getRoundInput = () => {
-  return prompt("Your play?", "rock");
+	return prompt("Your play?", "Rock");
 };
 
 function calcRoundResults() {
-  playerSelection = getRoundInput();
-  computerSelection = computerPlay();
-  console.log("Comp says: " + computerSelection);
-  switch (playerSelection.toLowerCase()) {
-    case "rock":
-      switch (computerSelection.toLowerCase()) {
-        case "rock":
-          return "Equals";
-        case "paper":
-          return "You lose";
-        case "scissors":
-          return "You win";
-      }
-      break;
-    case "paper":
-      switch (computerSelection.toLowerCase()) {
-        case "paper":
-          return "Equals";
-        case "rock":
-          return "You win";
-        case "scissors":
-          return "You lose";
-      }
-      break;
-    case "scissors":
-      switch (computerSelection.toLowerCase()) {
-        case "scissors":
-          return "Equals";
-        case "paper":
-          return "You win";
-        case "rock":
-          return "You lose";
-      }
-      break;
-  }
+	playerSelection = getRoundInput();
+	computerSelection = computerPlay();
+	console.log("Comp says: " + computerSelection);
+	switch (playerSelection.toLowerCase()) {
+		case "rock":
+			switch (computerSelection.toLowerCase()) {
+				case "rock":
+					return "Equals";
+				case "paper":
+					return "You lose";
+				case "scissors":
+					return "You win";
+			}
+			break;
+		case "paper":
+			switch (computerSelection.toLowerCase()) {
+				case "paper":
+					return "Equals";
+				case "rock":
+					return "You win";
+				case "scissors":
+					return "You lose";
+			}
+			break;
+		case "scissors":
+			switch (computerSelection.toLowerCase()) {
+				case "scissors":
+					return "Equals";
+				case "paper":
+					return "You win";
+				case "rock":
+					return "You lose";
+			}
+			break;
+	}
 }
 let calcScore = () => {
-  score = 0;
-  para = document.createElement("p");
-  document.querySelector("div").appendChild(para);
-  for (let i = 0; i < 5; i++) {
-    text = `This is round ${i}.`
-    document.querySelector("div").appendChild(para);
-    switch (calcRoundResults()) {
-      case "You win":
-        text = "Round won."
-        document.querySelector("div").appendChild(para);
-        score += 1;
-        break;
-      case "You lose":
-        text = "Round lost."
-        document.querySelector("div").appendChild(para);
-        score -= 1;
-        break;
-    }
-  }
-  return score;
-}
+	score = 0;
+	para = document.createElement("p");
+	div.appendChild(para);
+	for (let i = 0; i < 5; i++) {
+		text = `This is round ${i}.`;
+		div.appendChild(para);
+		switch (calcRoundResults()) {
+			case "You win":
+				text = "Round won.";
+				div.appendChild(para);
+				score += 1;
+				break;
+			case "You lose":
+				text = "Round lost.";
+				div.appendChild(para);
+				score -= 1;
+				break;
+		}
+	}
+	return score;
+};
 let showResults = () => {
-  para = document.createElement("p");
-  document.querySelector("div").appendChild(para);
+	para = document.createElement("p");
+	div.appendChild(para);
 
-  if (score > 0) {
-    text = `Your score is ${score}. You've won the series.`;
-    para.textContent = text;
-  } else if (score < 0) {
-    text = `Your score is ${score}. You've lost the series.`;
-    para.textContent = text;
-  } else {
-    text = `Your score is ${score}. There's no winner.`;
-    para.textContent = text;
-  }
-}
+	if (score > 0) {
+		text = `Your score is ${score}. You've won the series.`;
+		para.textContent = text;
+	} else if (score < 0) {
+		text = `Your score is ${score}. You've lost the series.`;
+		para.textContent = text;
+	} else {
+		text = `Your score is ${score}. There's no winner.`;
+		para.textContent = text;
+	}
+};
 let game = () => {
-  calcScore();
-  showResults();
-}
+	calcScore();
+	showResults();
+};
+
+let button = document.querySelector("button");
+button.addEventListener("click", function (e) {
+	game();
+});
