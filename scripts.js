cf25b8c341edd584c1f13f6a96a2ea25fe31c3f5
Lippiece <seriouss7222@gmail.com>
2022-04-03 15:17:09 +0200

    Assign numbers in computer play to the outputs

--- scripts.js
+++ scripts.js
@@ -16,6 +16,19 @@ let result;
 
 function getCompSelection() {
 	rand = Math.floor(Math.random() * 3);
+	switch (rand) {
+		case 0:
+			return "rock";
+			break;
+		case 1:
+			return "paper";
+			break;
+		case 2:
+			return "scissors";
+			break;
+	}
+}
+
 }
 
 
